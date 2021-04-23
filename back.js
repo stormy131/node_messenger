86a897bb774a91f6cde2868bebe2eb7d9db3ad78
stormy131 <artemkopyl1990@gmail.com>
2021-04-23 23:27:12 +0300

    made my api visible for other modules

--- back.js
+++ back.js
@@ -19,151 +19,163 @@ class Account {
   }
 }
 
-// TODO IN FRONT:
-// FIRSTLY, NEED TO CHECK/CREATE DATABASE IN DIRECTORY OF PROJECT
+class Api{
 
-const checkDB = () => new Promise(resolve => {
-  fs.mkdir('data', err => {
-    if (err) {
-      fs.readFile('data/authorize', err => {
+  // TODO IN FRONT:
+  // FIRSTLY, NEED TO CHECK/CREATE DATABASE IN DIRECTORY OF PROJECT
+
+  checkDB() {
+    return new Promise(resolve => {
+      fs.mkdir('data', err => {
         if (err) {
-          fs.writeFile('data/authorize', '', () => {});
-        }
-      });
+          fs.readFile('data/authorize', err => {
+            if (err) {
+              fs.writeFile('data/authorize', '', () => {});
+            }
+          });
 
-      fs.readFile('data/info', err => {
-        if(err){
-          fs.writeFile('data/info', '', () => {});
-        }
-      });
+          fs.readFile('data/info', err => {
+            if(err){
+              fs.writeFile('data/info', '', () => {});
+            }
+          });
 
-      fs.mkdir('data/messages', err => {
-        if(err) resolve();
-      });
+          fs.mkdir('data/messages', err => {
+            if(err) resolve();
+          });
 
-      return;
-    }
-
-    fs.writeFile('data/authorize', '', () => {});
-    fs.writeFile('data/info', '', () => {});
-    fs.mkdir('data/messages', () => {});
-    resolve();
-  });
-});
-
-const createAccount = (login, password) => new Promise((resolve, reject) => {
-  fs.readFile('data/authorize', (err, data) => {
-    if (err){
-      console.log('CHECK YOUR DB!');
-      throw err;
-    }
-
-    const users = data.toString().split('\n');
-    users.pop();
-    console.log(users);
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i].split(' - ');
-      if (login === user[0]){
-        console.log('LOGIN IS ALREADY USED');
-        resolve();
-        return;
-      }
-    }
-
-    fs.appendFile('data/authorize', login + ' - ' + password + '\n', err => {
-      if (err){
-        console.log('SMTH WENT WRONG WHILE APPENDING DATA');
-        reject();
-      }
-
-      console.log('created');
-      resolve();
-    });
-  });
-});
-
-const checkAccount = (login, password) => new Promise( resolve => {
-  fs.readFile('data/authorize', (err, data) => {
-    if (err){
-      console.log('CHECK DB');
-      resolve();
-      return;
-    }
-
-    const users = data.toString().split('\n');
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i].split(' - ');
-      if (login === user[0] && password === user[1]){
-        console.log('ACC EXISTS');
-        resolve(true);
-        return;
-      }
-    }
-
-    console.log('INCORRECT USER DATA');
-    resolve(false);
-  });
-});
-
-// INFO-ARRAY INTERFACE: [FULL NAME, AGE, COUNTRY, CITY, INFO]
-
-const addInfo = async (login, info) => {
-  const user = new Account(login, ...info);
-  await fs.appendFile('data/info', JSON.stringify(user) + '\n', err => {
-    if (err) {
-      console.log('SMTH went wrong while appending data');
-      return;
-    }
-  });
-};
-
-const getInfo = async login => {
-  await fs.readFile('data/info', (err, data) => {
-    if (err) {
-      console.log('CHECK YOUR DB');
-      return;
-    }
-
-    const content = data.toString().split('\n');
-    content.pop();
-    for (const line of content) {
-      const user = JSON.parse(line);
-      if (user.login === login) {
-        console.dir(user);
-      }
-    }
-  });
-};
-
-const addMessage = async (from, to, message) => {
-  fs.readFile('data/messages/' + from + ' - ' + to, err => {
-    if(err){
-      fs.readFile('data/messages/' + to + ' - ' + from, err => {
-        if(err){
-          fs.writeFile('data/messages/' + from + ' - ' + to,
-                       from + ': ' + message + '\n', () => {});
           return;
         }
 
-        fs.appendFile('data/messages/' + to + ' - ' + from,
-                      from + ': ' + message + '\n', () => {});
-        return;
+        fs.writeFile('data/authorize', '', () => {});
+        fs.writeFile('data/info', '', () => {});
+        fs.mkdir('data/messages', () => {});
+        resolve();
       });
-    }
+    });
+  }
 
-    fs.appendFile('data/messages/' + from + ' - ' + to,
-                  from + ': ' + message + '\n', () => {});
-  });
+  createAccount(login, password){ 
+    return new Promise((resolve, reject) => {
+      fs.readFile('data/authorize', (err, data) => {
+        if (err){
+          console.log('CHECK YOUR DB!');
+          throw err;
+        }
+
+        const users = data.toString().split('\n');
+        users.pop();
+        console.log(users);
+        for (let i = 0; i < users.length; i++) {
+          const user = users[i].split(' - ');
+          if (login === user[0]){
+            console.log('LOGIN IS ALREADY USED');
+            resolve();
+            return;
+          }
+        }
+
+        fs.appendFile('data/authorize', login + ' - ' + password + '\n', err => {
+          if (err){
+            console.log('SMTH WENT WRONG WHILE APPENDING DATA');
+            reject();
+          }
+
+          console.log('created');
+          resolve();
+        });
+      });
+    });
+  }
+
+  checkAccount(login, password){ 
+    return new Promise( resolve => {
+      fs.readFile('data/authorize', (err, data) => {
+        if (err){
+          console.log('CHECK DB');
+          resolve();
+          return;
+        }
+
+        const users = data.toString().split('\n');
+        for (let i = 0; i < users.length; i++) {
+          const user = users[i].split(' - ');
+          if (login === user[0] && password === user[1]){
+            console.log('ACC EXISTS');
+            resolve(true);
+            return;
+          }
+        }
+
+        console.log('INCORRECT USER DATA');
+        resolve(false);
+      });
+    });
+  }
+
+  // INFO-ARRAY INTERFACE: [FULL NAME, AGE, COUNTRY, CITY, INFO]
+
+  async addInfo(login, info){
+    const user = new Account(login, ...info);
+    await fs.appendFile('data/info', JSON.stringify(user) + '\n', err => {
+      if (err) {
+        console.log('SMTH went wrong while appending data');
+        return;
+      }
+    });
+  }
+
+  async getInfo(login){
+    await fs.readFile('data/info', (err, data) => {
+      if (err) {
+        console.log('CHECK YOUR DB');
+        return;
+      }
+
+      const content = data.toString().split('\n');
+      content.pop();
+      for (const line of content) {
+        const user = JSON.parse(line);
+        if (user.login === login) {
+          console.dir(user);
+        }
+      }
+    });
+  }
+
+  async addMessage(from, to, message){
+    fs.readFile('data/messages/' + from + ' - ' + to, err => {
+      if(err){
+        fs.readFile('data/messages/' + to + ' - ' + from, err => {
+          if(err){
+            fs.writeFile('data/messages/' + from + ' - ' + to,
+                        from + ': ' + message + '\n', () => {});
+            return;
+          }
+
+          fs.appendFile('data/messages/' + to + ' - ' + from,
+                        from + ': ' + message + '\n', () => {});
+          return;
+        });
+      }
+
+      fs.appendFile('data/messages/' + from + ' - ' + to,
+                    from + ': ' + message + '\n', () => {});
+    });
+  }
+
 }
+
+module.exports = Api;
 
 //TESTING
 
 (async () => {
-  await checkDB();
-  await createAccount('Artem', '!@#');
-  await checkAccount('Artem', '!@#');
-  await addInfo('Artem', ['1','2','3','4','5']);
-  await getInfo('Artem');
-  await addMessage('Artem', 'Dmytro', 'Welcome to node_messanger!)');
+  const api = new Api;
+  await api.checkDB();
+  await api.createAccount('Artem', '!@#');
+  await api.checkAccount('Artem', '!@#');
+  await api.addInfo('Artem', ['1','2','3','4','5']);
+  await api.getInfo('Artem');
+  await api.addMessage('Artem', 'Dmytro', 'Welcome to node_messanger!)');
 })();
-
